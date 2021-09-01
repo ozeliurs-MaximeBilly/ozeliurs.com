fdff4a0332bfe12d50632d969e2bdf94ddafbca5
BILLY Maxime <ozeliurs@gmail.com>
2021-09-01 18:17:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ def article(name):
     return render_template("article.html", article=html)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
