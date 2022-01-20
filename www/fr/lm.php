ca27b90d9b29840dd4c3fcdb5d96af1e8e348447
Ozeliurs <ozeliurs@gmail.com>
2022-01-20 14:34:25 +0100

    grega

--- www/fr/lm.php
+++ www/fr/lm.php
@@ -3,5 +3,8 @@ include '../utils.inc.php';
 head("Ozeliurs' Website");
 start_page();
 include_once 'lm_fr.php';
+?>
+<a href="/fr/lm_print.php?entreprise=<?php echo $_GET["entreprise"];?>&entreprise_interet=<?php echo $_GET["entreprise_interet"];?>"></a>
+<?php
 end_page();
 ?>
